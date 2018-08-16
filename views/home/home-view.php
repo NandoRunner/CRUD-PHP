97dd1df6a01f376ad9d0262351735ce3d4a12b92
Fernando Andrade <nando.az@gmail.com>
2018-08-16 08:39:16 -0300

    Initial

---
+++ views/home/home-view.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('ABSPATH')) exit; ?>
+
+
+<div class="wrap">
+
+	<p>Olá, <br>
+	Se você está visualizando esta página, seu sistema PHP com padrão MVC está funcionando.</p>
+
+	<p>Não deixe de visitar nosso site para dicas <a href="http://www.tutsup.com">tutsup.com</a>.
+    
+    <p> Tutorial 	
+    <a href="https://www.todoespacoonline.com/w/2014/09/mvc-em-php-parte-1/">Utilizando estrutura MVC em PHP</a>
+    
+</div> <!-- .wrap -->
